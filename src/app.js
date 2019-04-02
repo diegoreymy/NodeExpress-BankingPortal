a0d859997742428833728098c81a88a659685c4f
Diego Reymy <diegoreymy@gmail.com>
2019-04-02 16:55:38 -0300

    Configurando ambiente heroku

--- src/app.js
+++ src/app.js
@@ -37,5 +37,5 @@ app.get('/profile', (req, res) => {
 process.env.PORT = process.env.PORT || 3000; // Si existe un puerto en el process enviroment usa ese, si no, usa el 3000.
 
 app.listen(process.env.PORT, () => {
-    console.log('PS Project Running on port 3000!');
+    console.log(`PS Project Running on port ${process.env.PORT}!`);
 });
